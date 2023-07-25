0cc41aa6a64f0a60138a2cc6a909e4ce757cb15c
savragon <suryawork2018@gmail.com>
2023-07-25 10:31:42 -0400

    fortnite

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,3 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
-ReactDOM.createRoot(dsocument.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
